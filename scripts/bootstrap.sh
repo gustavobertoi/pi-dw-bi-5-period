e0d9c633d14a63f4619d97667e24433315628521
Gustavo Bertoi <gustavobertoi@outlook.com>
2023-05-07 19:51:59 -0300

    refactor: inline docker mysql run

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,2 @@
 docker volume create --driver local pi-dw-bi-5-period-mysql-volume
-
-docker run -d \
--e MYSQL_ROOT_PASSWORD=$DATABASE_PASS \
--e MYSQL_DATABASE=$DATABASE_NAME \
--p 3306:3306 \
--v pi-dw-bi-5-period-mysql-volume:/var/lib/mysql \
-mysql:5.7
+docker run -d -e MYSQL_ROOT_PASSWORD=$DATABASE_PASS -e MYSQL_DATABASE=$DATABASE_NAME -p 3306:3306 -v pi-dw-bi-5-period-mysql-volume:/var/lib/mysql mysql:5.7
