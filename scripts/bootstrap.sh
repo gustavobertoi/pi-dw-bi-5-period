c51e23db3ab12143eb0bf18fdc0fbe7d0188d9de
Gustavo Bertoi <gustavobertoi@outlook.com>
2023-05-07 19:49:12 -0300

    fixing bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,8 @@
 docker volume create --driver local pi-dw-bi-5-period-mysql-volume
 
 docker run -d \
--e MYSQL_ROOT_PASSWORD=$(echo $DATABASE_USER) \
--e MYSQL_DATABASE=$(echo $DATABASE_PASS) \
+-e MYSQL_ROOT_PASSWORD=$DATABASE_USER \
+-e MYSQL_DATABASE=$DATABASE_PASS \
 -p 3306:3306 \
 -v pi-dw-bi-5-period-mysql-volume:/var/lib/mysql \
 mysql:5.7
